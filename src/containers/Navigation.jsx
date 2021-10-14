024d078ae95158e9316994d31237e2aadddb748c
Alex <suhobski@mail.ru>
2021-10-14 15:18:23 +0300

    feat: button Create product

--- src/containers/Navigation.jsx
+++ src/containers/Navigation.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router';
 
 const NavigationWrap = styled('nav')({
+  padding: '0.5rem',
   border: '1px solid #eeeeee',
 });
 
-const Navigation = () => (
-  <NavigationWrap>
-    <p>nav</p>
-  </NavigationWrap>
-);
+const ButtonAddProduct = styled('button')({
+  padding: '0.5rem',
+  border: 'none',
+  outline: 'none',
+  borderRadius: 4,
+  background: '#b8f2c8',
+  '&:active': {
+    background: '#a9deb7',
+  },
+});
+
+const Navigation = () => {
+  const history = useHistory();
+
+  return (
+    <NavigationWrap>
+      <ButtonAddProduct
+        onClick={() => history.push('/create-product')}
+        type="button"
+      >
+        Create product
+      </ButtonAddProduct>
+    </NavigationWrap>
+  );
+};
 
 export default Navigation;
