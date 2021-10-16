6b4af6fd878c94569f73fbf643a90e7be8418856
Alex <suhobski@mail.ru>
2021-10-16 08:58:01 +0300

    feat: add build version

--- README.md
+++
@@ -1,3 +0,0 @@
-# Exore LTD Test
-
-https://exore.pro/en/testovoe-zadanie-na-pozicziyu-react-frontend-developer
--- src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import catalogReducer from './catalog';
-import filterReducer from './filter';
-
-export default combineReducers({
-  catalog: catalogReducer,
-  filter: filterReducer,
-});
